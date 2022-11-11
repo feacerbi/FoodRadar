42f969798bdc89df6c4ee00e0988673f77620ca7
Felipe Acerbi <feac@hellofresh.com>
2022-11-11 01:43:07 +0100

    Remove unused plugins module

--- build-setup/build.gradle.kts
+++ build-setup/build.gradle.kts
@@ -2,13 +2,7 @@ plugins {
     `kotlin-dsl`
 }
 
-group = "com.felipeacerbi.buildsetup"
-
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
-}
-
-dependencies {
-    compileOnly(libs.android.gradlePlugin)
 }
--- build-setup/settings.gradle.kts
+++ build-setup/settings.gradle.kts
@@ -13,5 +13,3 @@ dependencyResolutionManagement {
         }
     }
 }
-
-include(":plugins")
